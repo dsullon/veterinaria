5be54cf9c7dcdae0ea3b211e1c1a3291caae854c
Oscar Leiva <leivaoscar2010@gmail.com>
2014-10-02 00:43:28 -0500

    actualizando cambios

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140928022224) do
-=======
-ActiveRecord::Schema.define(version: 20140927224257) do
-
 
   create_table "breeds", force: true do |t|
     t.string   "name"
@@ -23,15 +19,12 @@ ActiveRecord::Schema.define(version: 20140927224257) do
     t.datetime "updated_at"
   end
 
-
   create_table "service_types", force: true do |t|
     t.boolean  "atHome"
     t.boolean  "inSite"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-
 
   create_table "services", force: true do |t|
     t.string   "name"
