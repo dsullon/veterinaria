14b75a80c2607496e07abeee982acba556f82e68
Donald A. Sullon <alexsullonporras@hotmail.com>
2014-10-05 20:01:27 -0500

    Creacion del login de usuarios yh veterinarias

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-
-  devise_for :vets, controllers:{registrations:'vets'}
-  devise_for :users, controllers:{registrations:'users'}
+  devise_for :vets , controllers:{registrations:'vets/registrations'}
+  devise_for :users , controllers:{registrations:'users/registrations'}
   resources :pets
   
 
--- test/helpers/vets_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class VetsHelperTest < ActionView::TestCase
-end
