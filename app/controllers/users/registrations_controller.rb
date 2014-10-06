7f2f5833b1755601e771ea9289c2459d029ec314
Donald A. Sullon <alexsullonporras@hotmail.com>
2014-10-06 15:35:45 -0500

    Se agrego el layout a las paginas de modificacion de datos

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
  #before_filter :configure_sign_up_params, only: [:create]
  #before_filter :configure_account_update_params, only: [:update]
-
+ layout 'plantilla'
  before_filter :configure_permitted_parameters
 
   protected
